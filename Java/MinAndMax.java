433328745d8457b5e7307cdc4e9cc9f6bda8e17f
Siam <siam48444@gmail.com>
2025-07-05 22:24:36 +0600

    siam

--- Java/MinAndMax.java
+++ Java/MinAndMax.java
@@ -4,11 +4,22 @@ import java.util.Arrays;
 public class SecondLargest {
     public static void main(String[] args) {
         int[] arr = {5, 8, 12, 3, 9};
+
+        int small = min(arr);
+        System.out.println(small);
     }
 
 
     // Finds the minimum number is an array
     public static int min(int[] arr) {
-        
+        int smallest = Integer.MAX_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < smallest) {
+                smallest = arr[i];
+            }
+        }
+
+        return smallest;
     }
 }
