afbd28c07a4702589c74122acf16748624d83c33
Siam <siam48444@gmail.com>
2025-07-05 22:02:10 +0600

    siam

---
+++ Java/NthLargestInArray.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+
+public class NthLargestInArray {
+    public static void main(String[] args) {
+        int[] arr = {5, 8, 12, 3, 9};
+    }
+
+
+    public static int nthLargestNumber(int[] arr, int n) {
+        int 
+    }
+}
