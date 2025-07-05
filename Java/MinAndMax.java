5ed5f5ddec9a754876b3765066deb2b7d0b6ca73
Siam <siam48444@gmail.com>
2025-07-05 22:22:20 +0600

    siam

---
+++ Java/MinAndMax.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+
+public class SecondLargest {
+    public static void main(String[] args) {
+        int[] arr = {5, 8, 12, 3, 9};
+    }
+
+
+    // Finds the minimum number is an array
+    public static int min(int[] arr) {
+        
+    }
+}
--- Java/__Main.java
+++ Java/__Main.java
@@ -1,4 +1,4 @@
-public class Main {
+public class __Main {
 	public static void main(String[] args) {
 		
 	}
