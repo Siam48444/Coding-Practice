ca696ddff16acc877938ddc423bdae134263f00d
Siam <siam48444@gmail.com>
2025-07-05 22:12:19 +0600

    siam

--- Java/SecondLargest.java
+++ Java/SecondLargest.java
@@ -4,13 +4,23 @@ import java.util.Arrays;
 public class SecondLargest {
     public static void main(String[] args) {
         int[] arr = {5, 8, 12, 3, 9};
+
+        int secondLargest = secondLargestNumber(arr);
+        System.out.println(secondLargest);
     }
 
 
-    public static double secondLargestNumber(int[] arr) {
-        int track = 0;
+    public static int secondLargestNumber(int[] arr) {
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > )
+            if (arr[i] > largest) {
+                secondLargest = largest;
+                largest = arr[i];
+            }
         }
+
+        return secondLargest;
     }
 }
