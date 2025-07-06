6f9724e7e4be933acfa355be322e1896fa3053e0
Siam <siam48444@gmail.com>
2025-07-06 12:13:19 +0600

    siam

--- Java/MiniCalculator.java
+++ Java/MiniCalculator.java
@@ -1,5 +1,39 @@
 public class MiniCalculator {
     public static void main(String[] args) {
-        
+        add(5, 6);
+        subtract(5, 6);
+        multiply(5, 6);
+        divide(5, 6);
+    }
+
+
+    // Adds two integers
+    public static void add(int a, int b) {
+        System.out.printf("%d + %d = %d %n", a, b, a + b);
+    }
+
+
+    // Subtracts second integer from the first one
+    public static void subtract(int a, int b) {
+        System.out.printf("%d - %d = %d %n", a, b, a - b);
+    }
+
+
+    // Multiplies two integers
+    public static void multiply(int a, int b) {
+        System.out.printf("%d * %d = %d %n", a, b, a * b);
+    }
+
+
+    // Divides two valid integers
+    public static void divide(int a, int b) {
+        if (b != 0) {
+            double x = (double) a;
+            double y = (double) b;
+            System.out.printf("%d / %d = %f %n", a, b, x / y);
+        }
+        else {
+            System.out.println("Division by zero is not allowed.");
+        }
     }
 }
