9c98ad5be4f915626ca3d22ca62d549867d64795
Siam <siam48444@gmail.com>
2025-07-06 19:25:26 +0600

    siam

--- Java/_3110_ScoreOfString.java
+++ Java/_3110_ScoreOfString.java
@@ -1,9 +1,16 @@
-class _3110_ScoreOfString {
-    public void main(String[] args) {
-
+public class _3110_ScoreOfString {
+    public static void main(String[] args) {
+        String s = "hello";
+        System.out.println(scoreOfString(s));
     }   
 
-    public int scoreOfString(String s) {
+    public static int scoreOfString(String s) {
+        int answer = 0;
         
+        for (int i = 0; i < s.length() - 1; i++) {
+            answer += Math.abs(s.charAt(i) - s.charAt(i + 1));
+        }
+        
+        return answer;
     } 
 }
