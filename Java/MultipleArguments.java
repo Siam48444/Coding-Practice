5bb8fbb1c19f91a00e28cd79f22ed15d79e6ef54
Siam <siam48444@gmail.com>
2025-07-07 18:37:44 +0600

    siam

--- Java/MultipleArguments.java
+++ Java/MultipleArguments.java
@@ -1,5 +1,15 @@
 public class MultipleArguments {
   public static void main(String[] args) {
-     
+    System.out.println(sumNumbers(2, 3, 4));
+    System.out.println(sumNumbers(10));
+    System.out.println(sumNumbers());
+  }
+
+  public static int sumNumbers(int... numbers) {
+    int sum = 0;
+    for (int n : numbers) {
+      sum += n;
+    }
+    return sum;
   }
 }
