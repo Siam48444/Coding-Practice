631696842f227eb096a5691f6923a7861e5553f8
Siam <siam48444@gmail.com>
2025-07-07 18:47:46 +0600

    siam

--- Java/MultipleArguments.java
+++ Java/MultipleArguments.java
@@ -3,6 +3,9 @@ public class MultipleArguments {
     System.out.println(sumNumbers(2, 3, 4));
     System.out.println(sumNumbers(10));
     System.out.println(sumNumbers());
+
+    System.out.println(findMax(5, 6, 4));
+    System.out.println(findMax(-7, -5, 0));
   }
 
   public static int sumNumbers(int... numbers) {
@@ -12,4 +15,14 @@ public class MultipleArguments {
     }
     return sum;
   }
+
+  public static int findMax(int... numbers) {
+    int max = Integer.MIN_VALUE;
+    for (int n : numbers) {
+      if (n > max) {
+        max = n;
+      }
+    }
+    return max;
+  } 
 }
