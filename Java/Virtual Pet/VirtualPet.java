d2a3b86b9682768daec46c6689c338c8dcfcf646
Siam <siam48444@gmail.com>
2025-07-08 18:12:06 +0600

    siam

--- Java/Virtual Pet/VirtualPet.java
+++ Java/Virtual Pet/VirtualPet.java
@@ -1,10 +1,22 @@
 public class VirtualPet {
-    public String name;
+    private String name;
     private int hunger = 0;
     private int happiness = 0;
 
     VirtualPet(String name) {
         this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getHunger() {
+        return this.hunger;
+    }
+
+    public int getHappiness() {
+        return this.happiness;
     }
 
     public void feed() {
