c9a527078cddf3dc74a87a3a41f1e4c1aedeac47
Siam <siam48444@gmail.com>
2025-07-08 18:16:51 +0600

    siam

--- Java/Virtual Pet/VirtualPet.java
+++ Java/Virtual Pet/VirtualPet.java
@@ -20,34 +20,35 @@ public class VirtualPet {
     }
 
     public void feed() {
-        if (this.hunger > 0) {
-            this.hunger -= 1;
-        } 
+        if (this.hunger > 0) this.hunger -= 1; 
     }
 
     public void play() {
-        if (this.happiness < 10) {
-            this.happiness += 1;
-        }
+        if (this.happiness < 10) this.happiness += 1;
     }
 
     public void passTime() {
-        if (this.hunger < 10) {
-            this.hunger += 1;
-        }
-        if (this.happiness > 0) {
-            this.happiness -= 1;
-        }
+        if (this.hunger < 10) this.hunger += 1;
+        if (this.happiness > 0) this.happiness -= 1;
+    }
+
+    public String getMood() {
+        if (happiness >= 8) return "Very Happy";
+        else if (happiness >= 5) return "Content";
+        else if (happiness >= 3) return "Meh";
+        else return "Sad";
     }
 
     public void status() {
         String line = "--------------------";
+
         System.out.println(line);
         System.out.println("    Pet Status");
         System.out.println(line);
         System.out.println("Name      : " + this.name);
         System.out.println("Hunger    : " + this.hunger);
         System.out.println("Happiness : " + this.happiness);
+        System.out.println("Mood      : " + this.getMood());
         System.out.println(line);
     }
 }
