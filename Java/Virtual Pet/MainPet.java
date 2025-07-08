c28ca718a3d30be392208e70784f99529ff4eb88
Siam <siam48444@gmail.com>
2025-07-08 17:44:54 +0600

    sism

--- Java/Virtual Pet/MainPet.java
+++ Java/Virtual Pet/MainPet.java
@@ -1,5 +1,9 @@
 public class MainPet {
     public static void main(String[] args) {
+        VirtualPet pet1 = new VirtualPet("Tom");
         
+        pet1.feed();
+        pet1.play();
+        pet1.status();
     }
 }
--- Java/Virtual Pet/VirtualPet.java
+++ Java/Virtual Pet/VirtualPet.java
@@ -2,9 +2,20 @@ public class VirtualPet {
     public String name;
     private int hunger = 0;
     private int happiness = 0;
-    
 
     VirtualPet(String name) {
         this.name = name;
+    }
+
+    public void feed() {
+        this.hunger += 1;
+    }
+
+    public void play() {
+        this.happiness += 1;
+    }
+
+    public void status() {
+        System.out.printf("Name: %s\nHunger: %s\nHappiness: %s", this.name, this.hunger, this.happiness);
     }
 }
