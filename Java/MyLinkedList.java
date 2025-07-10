f84bdd69a3546e0ce700da6818c5a2e080f8b5b4
Siam <siam48444@gmail.com>
2025-07-10 16:10:02 +0600

    siam

--- Java/MyLinkedList.java
+++ Java/MyLinkedList.java
@@ -1,5 +1,15 @@
 public class MyLinkedList {
+    class Node {
+        String data;
+        Node next;
+
+        Node(String data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
     public static void main(String[] args) {
-        
+
     }
 }
