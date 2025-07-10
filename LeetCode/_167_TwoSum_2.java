d58c1f9c995d725ceda4b9f076ac682b269fffcc
Siam <siam48444@gmail.com>
2025-07-10 14:59:16 +0600

    siam

--- LeetCode/_167_TwoSum_2.java
+++ LeetCode/_167_TwoSum_2.java
@@ -3,7 +3,9 @@ import java.util.Arrays;
 
 class _167_TwoSum_2 {
     public static void main(String[] args) {
-
+        System.out.println(Arrays.toString(twoSum_1(new int[] {2, 7, 11, 15}, 9)));
+        System.out.println(Arrays.toString(twoSum_1(new int[] {2, 3, 4}, 6)));
+        System.out.println(Arrays.toString(twoSum_1(new int[] {-1, 0}, -1)));
     }
 
 
