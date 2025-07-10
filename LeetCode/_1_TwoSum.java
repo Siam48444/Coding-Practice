1ed19cb61dd1fccb5fece582a522b9acd307eb8b
Siam <siam48444@gmail.com>
2025-07-10 12:01:01 +0600

    siam

--- LeetCode/_1_TwoSum.java
+++ LeetCode/_1_TwoSum.java
@@ -1,9 +1,18 @@
+import java.util.HashMap;
+
 class _1_TwoSum {
     public static void main(String[] args) {
+        int[] nums1 = {2,7,11,15};
+        int target1 = 9;
 
+        int[] nums2 = {3,2,4};
+        int target2 = 6;
+
+        int[] nums3 = {3,3};
+        int target3 = 9;
     }
 
-    public int[] twoSum(int[] nums, int target) {
+    public int[] twoSum_1(int[] nums, int target) {
         
     }
 }
