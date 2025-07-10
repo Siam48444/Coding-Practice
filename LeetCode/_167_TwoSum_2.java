e255f5af669c9882e754ed131393fb97d2d92bc4
Siam <siam48444@gmail.com>
2025-07-10 15:13:18 +0600

    siam

--- LeetCode/_167_TwoSum_2.java
+++ LeetCode/_167_TwoSum_2.java
@@ -9,8 +9,25 @@ class _167_TwoSum_2 {
     }
 
 
-    // Time Complexity = O(), Space Complexity = O()
-    public int[] twoSum_1(int[] numbers, int target) {
-        
+    // Time Complexity = O(n), Space Complexity = O(1)
+    public static int[] twoSum_1(int[] numbers, int target) {
+        int left = 0, right = numbers.length - 1; 
+
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+
+            if (sum == target) {
+                return new int[] {left + 1, right + 1};
+            }
+
+            if (sum > target) {
+                right -= 1;
+            }
+            else {
+                left += 1;
+            }
+        }
+
+        return new int[] {-1, -1};
     }
 }
