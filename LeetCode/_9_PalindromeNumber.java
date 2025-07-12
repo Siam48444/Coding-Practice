beebad93e5760f1218d9f7d8fd9f6f92e13b9c7a
Siam <siam48444@gmail.com>
2025-07-12 17:01:06 +0600

    siam

--- LeetCode/_9_PalindromeNumber.java
+++ LeetCode/_9_PalindromeNumber.java
@@ -1,5 +1,12 @@
 class _9_PalindromeNumber {
     public static void main(String[] args) {
+        System.out.println(isPalindrome(121));
+        System.out.println(isPalindrome(-121));
+        System.out.println(isPalindrome(10));
+    }
+
+
+    public static boolean isPalindrome(int x) {
         
     }
 }
