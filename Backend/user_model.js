590ee96b59dd923d4920e99ebf445ce2aef4dd99
Siam <siam48444@gmail.com>
2025-07-15 11:34:22 +0600

    siam

--- Backend/user_model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const userSchema = mongoose.Schema({
-    name: String,
-    username: String,
-    email: String,
-});
-
-module.exports = mongoose.model("user", userSchema);
