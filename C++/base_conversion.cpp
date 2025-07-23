e528bb4dcf960aedf55b2890d2d39824baa7a490
Siam <siam48444@gmail.com>
2025-07-23 17:02:34 +0600

    siam

--- C++/base_conversion.cpp
+++ C++/base_conversion.cpp
@@ -2,23 +2,36 @@
 
 
 
-int decimal_to_binary(int dec) {
-    int bin = 0;
-    int place = 1;
+// Function to convert a decimal number to binary (as an int)
+int decimal_to_binary(int decimal) {
+    int binary = 0;
+    int position = 1;
 
-    while (dec > 0) {
-        int rem = dec % 2;
-        dec /= 2;
-        bin += rem * place;
-        place *= 10;
+    while (decimal > 0) {
+        int remainder = decimal % 2;
+        decimal /= 2;
+
+        binary += remainder * position;
+        position *= 10;
     }
 
-    return bin;
+    return binary;
 }
 
 
-int binary_to_decimal(int bin) {
+int binary_to_decimal(int binary) {
+    int decimal = 0;
+    int power = 1;
+    
+    while (binary > 0) {
+        int remainder = binary % 2;
+        binary /= 10;
 
+        decimal += remainder * power;
+        power *= 2;
+    }
+
+    return decimal;
 }
 
 
