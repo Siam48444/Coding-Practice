376b3e256e19dcf5042849af14913e617b887d36
Siam <siam48444@gmail.com>
2025-07-23 17:20:40 +0600

    siam

--- C++/base_conversion.cpp
+++ C++/base_conversion.cpp
@@ -2,23 +2,24 @@
 
 
 
-// Function to convert a decimal number to binary (as an int)
+// Function to convert a decimal number to binary
 int decimal_to_binary(int decimal) {
     int binary = 0;
     int position = 1;
-
+    
     while (decimal > 0) {
         int remainder = decimal % 2;
         decimal /= 2;
-
+        
         binary += remainder * position;
         position *= 10;
     }
-
+    
     return binary;
 }
 
 
+// Function to convert a binary number to decimal
 int binary_to_decimal(int binary) {
     int decimal = 0;
     int power = 1;
@@ -35,9 +36,32 @@ int binary_to_decimal(int binary) {
 }
 
 
+// Function to convert a decimal number to octal
+int decimal_to_octal(int decimal) {
+    int octal = 0;
+    int position = 1;
+    
+    while (decimal > 0) {
+        int remainder = decimal % 8;
+        decimal /= 8;
+        
+        octal += remainder * position;
+        position *= 10;
+    }
+    
+    return octal;
+}
+
+
+
+
 int main() {
+    std::cout << "Decimal\t\tBinary\t\tOctal\n";
+    
     for (int i = 0; i <= 10; i++) {
-        std::cout << i << "  -->  " << decimal_to_binary(i) << "\n";
+        std::cout << i << "\t\t" 
+            << decimal_to_binary(i) << "\t\t" 
+            << decimal_to_octal(i) << "\n";
     }
 
     return 0;
